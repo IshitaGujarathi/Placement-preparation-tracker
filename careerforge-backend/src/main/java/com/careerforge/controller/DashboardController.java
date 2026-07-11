70fd0a83fa7034fea020e3961a0c8cdbbdba6f9b
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-07-11 12:33:28 +0530

    Fixed the backend

--- careerforge-backend/src/main/java/com/careerforge/controller/DashboardController.java
+++ careerforge-backend/src/main/java/com/careerforge/controller/DashboardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/dashboard")
+@RequestMapping("/dashboard")
 @RequiredArgsConstructor
 public class DashboardController {
 
