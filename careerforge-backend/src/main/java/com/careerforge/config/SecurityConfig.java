29843a5b3735d6a7ecf1b371fbf028bec8fca8ef
IshitaGujarathi <ishitagujarathi07@gmail.com>
2026-07-11 10:57:53 +0530

    Fix CORS

--- careerforge-backend/src/main/java/com/careerforge/config/SecurityConfig.java
+++ careerforge-backend/src/main/java/com/careerforge/config/SecurityConfig.java
@@ -33,9 +33,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
+                "https://placement-preparation-tracker-rho.vercel.app",
                 "https://placement-preparation-tra-git-0fe0fd-ishita-gujarathis-projects.vercel.app",
                 "https://placement-preparation-tracker.vercel.app"
         ));
